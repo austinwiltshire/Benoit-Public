453e17e45332d1affc1bb37ca55435e423a53112
austinwiltshire <austinwiltshire@b02e84a0-10da-c248-aa29-986ee55558fd>
2008-02-29 02:17:24 +0000

    worked on GoogleSoup and got Google website preliminary working.

--- WebStock/src/Website.py
+++ WebStock/src/Website.py
@@ -1,6 +1,7 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
 import re
+import datetime
 
 class Bloomberg(object):
     """ A Bloomberg is a stock information service provider.  It can host any number of "
@@ -12,57 +13,116 @@ class Website(Bloomberg):
     """ Website is a Bloomberg that gets its information from a particular website. """
     pass
 
+class GoogleSoup(object):
+    """ Helper for Google website """
+    
+    balanceSheetAnnualDiv = "balannualdiv"
+    balanceSheetQuarterlyDiv = "balinterimdiv"
+    cashFlowsAnnualDiv = "casannualdiv"
+    cashFlowsQuarterlyDiv = "casinterimdiv"
+    incomeStatementAnnualDiv = "incintirimdiv"
+    incomeStatementQuarterlyDiv = "incannualdiv"
+    
+    revenueRE = re.compile("Revenue")
+    
+    def __init__(self, soup):
+        self.quarterlyDates = []
+        self.annualDates = []
+        #assumes that across balance sheet, cash flows, etc... same dates are used.
+        
+        self.balanceSheetAnnualDiv = soup.find('div', id=balanceSheetAnnualDiv)
+        self.balanceSheetQuarterlyDiv = soup.find('div', id=balanceSheetQuarterlyDiv)
+        self.cashFlowsAnnualDiv = soup.find('div', id=cashFlowsAnnualDiv)
+        self.cashFlowsQuarterlyDiv = soup.find('div', id=cashFlowsQuarterlyDiv)
+        self.incomeStatementAnnualDiv = soup.find('div', id=incomeStatementAnnualDiv)
+        self.incomeStatementQuarterlyDiv = soup.find('div', id=incomeStatementQuarterlyDiv)
+        
+        self.revenueQuarterly = None
+        self.revenueAnnual = None
+        
+    def getRevenueQuarterly(self):
+        if self.revenueQuarterly:
+            return self.revenueQuarterly
+        
+        revenueQuarterly = self.incomeStatementQuarterlyDiv.find("...") #YOU ARE HERE
+            
+
 class Google(Website):
     def __init__(self):
-        cachedPages = {}
+        self.cachedPages = {}
+        
+    def buildURL(self, symbol):
+        baseURL = "http://finance.google.com/finance?q=%s" % symbol
+        page = urllib2.urlopen(baseURL)
+        page = BeautifulSoup(page)
+        
+        inc = re.compile("Income.*Statement")
+        links = page.findAll('a')
+        
+        suffix = filter(lambda x: x.string != None\
+                         and inc.search(x.string) != None,links)[0]['href']
+        return "".join(["http://finance.google.com/finance",suffix])
+    
+        #TODO: figure out 'find' syntax for lists.  there ought to be a find for lists.
+        #TODO: instead of filter, us decorate, find, undecorate.
+        #TODO: might be able to put this all in the page.findAll method call
     
     def getRevenue(self, symbol, type='annual', date=None):
         """ Returns the income statement as a dict for the symbol at the date.  If date is "
         " blank then returns all income statements available for the symbol in order """
         
         type = type.lower()
-        if type!='annual' or type!='quarterly':
+        if type!='annual' and type!='quarterly':
+            print type
             raise Exception("type needs to be annual or quarterly")
         
-        if symbol in cachedPages:
-            mypage = cachedPages[symbol]
+        if symbol in self.cachedPages:
+            mypage = self.cachedPages[symbol]
         else:
             buildURL = self.buildURL(symbol)
-            page = urllib2.urlopen(buildURL)
+            html = urllib2.urlopen(buildURL)
             mypage = BeautifulSoup(html)
-            cachedPages[symbol] = mypage
-        
-        divisions = mypage.findAll(lambda tag: tag.name == 'div' and tag.has_key('id'))
+            self.cachedPages[symbol] = mypage
         
         if type=='annual':
             divname = 'incannualdiv'
         if type=='quarterly':
             divname = 'incinterimdiv'
+            
+        div = mypage.find('div', id=divname)
         
-        
-        div = None
-        for division in divisions:
-            if division['id'] == divname:
-                div = division
+#        div = None
+ #       for division in divisions:
+  #          if division['id'] == divname:
+   #             div = division
         
         tds = div.findAll('td')[1:]
+        #TODO: replace this with a callable or something?
+        
         #get all td's, except the first because it is just 'in millions of USD ...'
         
         datere = re.compile(r"\d+ months Ending (?P<year>\d{4,4})-(?P<month>\d{2,2})-(?P<day>\d{2,2})")    
+
+        tds = [datere.search(td.b.string) for td in tds if td.b and td.b.string and datere.search(td.b.string)]
+        #TODO: should this be simplified in a loop or can I improve my div.findAll above to get this sort of stuff?
         
-        tds = map(lambda td: datere.search(td), tds)
-        dates = map(lambda td: datetime.date(td.group('year'), td.group('month'), td.group('day')))
+#        tds = map(lambda td: datere.search(td.string) if td.string != None else , tds)
+        dates = [datetime.date(int(td.group('year')), int(td.group('month')), int(td.group('day'))) for td in tds]
+#        dates = map(lambda td: datetime.date(td.group('year'), td.group('month'), td.group('day')), tds)
         #dates should be in order of most recent first and least recent last
         
-        trs = divisions.findAll('tr')[1:]
+        trs = div.findAll('tr')[1:]
         #get all trs, except the first because its just the dates.
         revenuere = re.compile("Revenue")
-        number = re.compile(r"[\d{3,3},]*\d{3,3}\.\d+")
+        number = re.compile(r"[\d{3,3},]*\d{0,3}\.\d+|-")
         
         for tr in trs:
             tds = tr.findAll('td')
-            if revenuere.search(tds.string) != None:
-                revenues = map(lambda td: number.search(td), tds[1:])
-                revenues = map(lambda r: r.replace(",",""), revenues)
-                revenues = map(lambda r: float(r), revenues)
-                return zip(tds[1:],dates)
+            if tds[0].string and revenuere.search(tds[0].string) != None:
+                    revenues = [number.search(r.string).group() for r in tds[1:] if number.search(r.string)]
+                    revenues = [r.replace(",","") for r in revenues]
+                    revenues = [float(r) if r != "-" else r for r in revenues]
+#                    revenues = map(lambda r: number.search(r.string), tds[1:])
+                    #revenues = map(lambda r: r.replace(",",""), revenues)
+                    #revenues = map(lambda r: float(r), revenues)
+                    return dict(zip(dates,revenues))
