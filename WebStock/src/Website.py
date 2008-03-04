3c57ae472e5ab7b7f311a4dcc27ea7dcc716ee5d
austinwiltshire <austinwiltshire@b02e84a0-10da-c248-aa29-986ee55558fd>
2008-03-04 02:32:39 +0000

    testing and integrating new xml schema model for google scraping.

--- WebStock/src/Website.py
+++ WebStock/src/Website.py
@@ -2,6 +2,7 @@ from BeautifulSoup import BeautifulSoup
 import urllib2
 import re
 import datetime
+import FinancialXML
 
 class Bloomberg(object):
     """ A Bloomberg is a stock information service provider.  It can host any number of "
@@ -16,8 +17,10 @@ class Website(Bloomberg):
 class GoogleSoup(object):
     """ Helper for Google website """
     
-    keywords = {"Revenue":({"Div":"IncomeStatement","Regex":r"(?<!(Other |Total ))Revenue"}),\
-                "OtherRevenue":({"Div":"IncomeStatement","Regex":r"Other Revenue, Total"})}
+    otherKW = FinancialXML.xml_to_dict("google.xml")
+    regexs = otherKW['regular_expressions']
+    divs = otherKW['divisions']
+    sec_docs = otherKW['sec_definition']
     
     #TODO: you are here.  i want to basically just create a table that will map different
     #attributes to their regex's, as well as define attributes automatically from this table
@@ -25,7 +28,7 @@ class GoogleSoup(object):
     #Google needs to inherit an interface similar to this one.  going to have to use 
     #getAttr[] heavily
     
-    numberRe = re.compile(r"[\d{3,3},]*\d{0,3}\.\d+|-")
+    numberRe = re.compile(r"-?[\d{3,3},]*\d{0,3}\.\d+|-")
     dateRe = re.compile(r"\d+ months Ending (?P<year>\d{4,4})-(?P<month>\d{2,2})-(?P<day>\d{2,2})")    
     
 
@@ -35,21 +38,31 @@ class GoogleSoup(object):
         self.annualDates = []
         #assumes that across balance sheet, cash flows, etc... same dates are used.
         
-        self.labels = {"BalanceSheet":{"Annual":soup.find('div', id='balannualdiv'),\
-                                      "Quarterly":soup.find('div', id='balinterimdiv')},\
-                       "IncomeStatement":{"Annual":soup.find('div', id='incannualdiv'),\
-                                          "Quarterly":soup.find('div', id='incinterimdiv')},\
-                       "CashFlowStatement":{"Annual":soup.find('div', id='casannualdiv'),\
-                                            "Quarterly":soup.find('div', id='casinterimdiv')},\
-                       "Dates":{"Annual":soup.find('div', id='incannualdiv'),\
-                                "Quarterly":soup.find('div', id='incinterimdiv')}}
+        self.labels = {"BalanceSheet":{"Annual":soup.find('div', id=self.divs['BalanceSheet']['Annual']),\
+                                      "Quarterly":soup.find('div', id=self.divs['BalanceSheet']['Quarterly'])},\
+                       "IncomeStatement":{"Annual":soup.find('div', id=self.divs['IncomeStatement']['Annual']),\
+                                          "Quarterly":soup.find('div', id=self.divs['IncomeStatement']['Quarterly'])},\
+                       "CashFlowStatement":{"Annual":soup.find('div', id=self.divs['CashFlowStatement']['Annual']),\
+                                            "Quarterly":soup.find('div', id=self.divs['CashFlowStatement']['Quarterly'])},\
+                       "Dates":{"Annual":soup.find('div', id=self.divs['Dates']['Annual']),\
+                                "Quarterly":soup.find('div', id=self.divs['Dates']['Quarterly'])}}
         
-        #TODO: put this in XML?
+        for name,searchRe in self.regexs.items():
+            self.addAttribute(name,searchRe)
+            
+    def getSecDoc(self, name):
+        if name in self.sec_docs['BalanceSheet']:
+            return 'BalanceSheet'
+        elif name in self.sec_docs['IncomeStatement']:
+            return 'IncomeStatement'
+        elif name in self.sec_docs['CashFlowStatement']:
+            return 'CashFlowStatement'
+        else:
+            raise Exception("Not a valid SEC identifier")
         
-        for name,arguments in self.keywords.items():
-            self.addAttribute(name,arguments)
+    def addAttribute(self, name, regEx):
         
-    def addAttribute(self, name, arguments):
+        secDoc = self.getSecDoc(name)
         
         annualMethodName = "getAnnual%s" % name
         quarterlyMethodName = "getQuarterly%s" % name
@@ -57,10 +70,10 @@ class GoogleSoup(object):
         annualVariableName = "annual%s" % name
         quarterlyVariableName = "quarterly%s" % name
         
-        annualDiv = self.labels[arguments['Div']]['Annual']
-        quarterlyDiv = self.labels[arguments['Div']]['Quarterly']
-        
-        searchRe = re.compile(arguments['Regex'])
+        annualDiv = self.labels[secDoc]['Annual']
+        quarterlyDiv = self.labels[secDoc]['Quarterly']
+            
+        searchRe = re.compile(regEx)
         
         annualMethod = lambda : self.webparse(annualVariableName, searchRe\
                                               , annualDiv, self.getAnnualDates())
@@ -79,21 +92,33 @@ class GoogleSoup(object):
         #get all trs, except the first because its just the dates.
         
         tds = None
-        for tr in trs:    
-            tds = tr.findAll('td')
+        for tr in trs:  
+            tds = tr.findAll("td")
+            if searchRe.search(str(tds[0])):
+                break
             if tds[0].string and searchRe.search(tds[0].string) != None:
                 break
-            if tds[0].b.string and searchRe.search(tds[0].b.string) != None: #important names are bolded
+            if tds[0].b and tds[0].b.string and searchRe.search(tds[0].b.string) != None: #important names are bolded
+                break
+            if tds[0].span and tds[0].span.string and searchRe.search(tds[0].span.string) != None:
+                break
+            if tds[0].b and tds[0].b.span and tds[0].b.span.string and searchRe.search(tds[0].b.span.string) != None:
                 break
             #TODO: 'find' syntax again
         
         values = []
         for result in tds[1:]:
-            #if there is no result, google tends to put a '-' after a 'span' tag
+            
+            #some random stuff is bolded.
+            if result.find("b"):
+                result = result.find("b")
+            
+            #if there is no result, google tends to put a '-' after a 'span' tag    
             if result.find("span"):
                 val = result.find("span")
             else:
                 val = result
+                
             
             #check using a regex whether there is a number or a '-' in this td
             match = self.numberRe.search(val.string)
@@ -179,7 +204,7 @@ class Google(Website):
         #get the keyword - strip off 'get' and 'quarterly' or 'annual'
         keyword = self.keywordRe.search(name).group('keyword') 
         
-        if GoogleSoup.keywords.has_key(keyword):
+        if GoogleSoup.regexs.has_key(keyword):
             return lambda *args: self.__myGetAttr__(name, *args)
         else: 
             raise AttributeError()
