24fbef2b10422b8899de122ad31b6179a6bffeb2
John <John@b02e84a0-10da-c248-aa29-986ee55558fd>
2008-08-14 00:32:40 +0000

    making sure i dont delete this.

--- WebStock/src/Experimental/BalanceSheet.py
+++ WebStock/src/Experimental/BalanceSheet.py
@@ -1,6 +1,13 @@
 # each class represents what?
 # it coudl be on symbol dates, so a quarterly balance sheet total assets might be one row in a database
 # or each class could be on the whoel thing, so one class would be 'balance sheet'
+from Signature import Signature, SignatureMap
+import datetime
+from Registry import Registry
+from Service import Service
+from elixir import *
+metadata.bind = "sqlite:///balancesheet.sqlite"
+metadata.bind.echo = True
 
 
 # going with:
@@ -8,27 +15,44 @@
 and Annual Balance Sheets.  A Balance sheet can be represented as a row in a database. """
 
 class Bloomberg(object):
-	pass
+	""" Provides functions for mapping "Hosts" to "Interfaces".  Hosts are things that say they can provide a certain service
+	given a certain contract, while Interfaces are items that need that service and also provide a certain contract/signature.
+	Bloomberg does the job of matching up interfaces to hosts """
 
 class BalanceSheet(Bloomberg):
+	""" Balance sheet contains ... well, balance sheet information.  There are two types, Quarterly and Annual, and this is just a 
+	semantic reference """
 	pass
 	
-
-class QuarterlyBalanceSheet(BalanceSheet):
-	Bloomberg.Interface(BalanceSheet, "CashAndEquivalents", Bloomberg.Signature(symbol, FinancialDate.Quarter))
+class QuarterlyBalanceSheet(BalanceSheet,Entity):
+	""" Represents a Quarterly Balance sheet and provides interface access to members of the quarterly balance sheet, closed over
+	Symbol and Date via the object's constructor """
+	
+	Symbol = Field(Unicode(10))
+	Date = Field(DateTime)
+	
+	def __init__(self, symbol, date):
+		super(QuarterlyBalanceSheet,self).__init__()
+		self.Symbol = symbol
+		self.Date = date
+	
+	_CashAndEquivalents = Field(Float(precision=4))
+	CashAndEquivalents = Registry.getService(Service("CashAndEquivalents",Signature((unicode,"symbol"),(datetime.date, "date"))),
+											 SignatureMap({"symbol":"Symbol", "date":"Date"}), _CashAndEquivalents)
+	
+	def __repr__(self):
+		return str(self)
+	
+	def __str__(self):
+		return "<Quarterly Balance Sheet for %s on %s>" % (self.Symbol, self.Date)
+	
+	#Registry.Interface(BalanceSheet, "CashAndEquivalents", Signature(("symbol",unicode),("date",datetime.date)))
 	#this above method should tell bloomberg that BalanceSheet requires someone to provide CashAndEquivalents
 	#it makes a contract saying it will provide the symbol and Quarter, and queries whether someone can provide
 	# that given the inputs
 	
+	
+	
 
 class AnnualBalanceSheet(object):
 	pass
-
-class PluginRegistry(type):
-	""" PluginRegistry metaclass takes in a class, inspects it's members and does a name lookup on them.  For members that it has
-	a plugin registered for, it adds properties for those names which link back to the registered plugin. """
-	def __new__(cls, name, bases, dct):
-		return super(PluginRegistry,cls).__new__(cls, name, bases, dct)
-	def __init__(cls, name, bases, dct):
-		return super(PluginRegistry,cls).__init__(cls, name, bases, dct)
-	
