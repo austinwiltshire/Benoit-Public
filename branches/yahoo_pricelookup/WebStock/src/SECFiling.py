ff93df2b3bf520a6c0ac577cc4602594ccd1b55a
austinwiltshire <austinwiltshire@b02e84a0-10da-c248-aa29-986ee55558fd>
2011-03-20 03:40:43 +0000

    need to make sure branch to branch is committed itself.

--- branches/yahoo_pricelookup/WebStock/src/SECFiling.py
+++
@@ -1,7 +0,0 @@
-""" This is a mess.  Combine with Bloomberg and attributes, as this logic is just all over the place.  This might dissapear completely with proper metaclasses. """
-
-
-from Bloomberg import PersistantHostMeta, Meta, Annual, Quarterly, Daily, PersistantHost, Require, Provide#, DecorateServices, ServicesDetected, PHMeta, BMeta, BAnnual, BQuarter, BDaily
-
-
-	
