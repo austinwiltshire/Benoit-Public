7faf43c1da219515f06f97fbbde29895d825c20f
austinwiltshire <austinwiltshire@b02e84a0-10da-c248-aa29-986ee55558fd>
2011-05-25 03:06:16 +0000

    updated tests to also test the calendar object

--- branches/financial_date/src/FinancialDateUnitTest.py
+++ branches/financial_date/src/FinancialDateUnitTest.py
@@ -22,6 +22,14 @@ class FinancialDateUnitTest(unittest.TestCase):
         Test a week in advance from a sunday
         """
         
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 0), datetime.datetime(2011, 5, 2))
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 1), datetime.datetime(2011, 5, 3))
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 2), datetime.datetime(2011, 5, 4))
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 3), datetime.datetime(2011, 5, 5))
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 4), datetime.datetime(2011, 5, 6))
+        
         self.assertEqual(FinancialDate.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 0), datetime.datetime(2011, 5, 2))
         self.assertEqual(FinancialDate.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 1), datetime.datetime(2011, 5, 3))
         self.assertEqual(FinancialDate.NthTradingDayAfter(datetime.datetime(2011, 5, 1), 2), datetime.datetime(2011, 5, 4))
@@ -32,6 +40,15 @@ class FinancialDateUnitTest(unittest.TestCase):
         """
         Test a week before from a saturday
         """
+        
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 0), datetime.datetime(2011, 4, 29))
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 1), datetime.datetime(2011, 4, 28))
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 2), datetime.datetime(2011, 4, 27))
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 3), datetime.datetime(2011, 4, 26))
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 4), datetime.datetime(2011, 4, 25))
+        
         self.assertEqual(FinancialDate.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 0), datetime.datetime(2011, 4, 29))
         self.assertEqual(FinancialDate.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 1), datetime.datetime(2011, 4, 28))
         self.assertEqual(FinancialDate.NthTradingDayBefore(datetime.datetime(2011, 4, 30), 2), datetime.datetime(2011, 4, 27))
@@ -43,12 +60,20 @@ class FinancialDateUnitTest(unittest.TestCase):
         Test that we find a friday before a sunday
         """
         
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.FirstTradingDayBefore(datetime.datetime(1983, 4, 17)), datetime.datetime(1983, 4, 15))
+        
         self.assertEqual(FinancialDate.FirstTradingDayBefore(datetime.datetime(1983, 4, 17)), datetime.datetime(1983, 4, 15))
         
     def test_after_saturday(self):
         """
         Test that we find a monday after a saturday
         """
+        
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.FirstTradingDayAfter(datetime.datetime(1998, 1, 31)), datetime.datetime(1998,2,2))
         
         self.assertEqual(FinancialDate.FirstTradingDayAfter(datetime.datetime(1998, 1, 31)), datetime.datetime(1998,2,2))
         
@@ -57,6 +82,11 @@ class FinancialDateUnitTest(unittest.TestCase):
         Test before date for floating holiday
         """
         
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(2002, 3, 29), 0), datetime.datetime(2002, 3, 28))
+        self.assertEqual(calendar.FirstTradingDayBefore(datetime.datetime(2002, 3, 29)), datetime.datetime(2002, 3, 28))
+        
         self.assertEqual(FinancialDate.NthTradingDayBefore(datetime.datetime(2002, 3, 29), 0), datetime.datetime(2002, 3, 28))
         self.assertEqual(FinancialDate.FirstTradingDayBefore(datetime.datetime(2002, 3, 29)), datetime.datetime(2002, 3, 28))
     
@@ -64,6 +94,11 @@ class FinancialDateUnitTest(unittest.TestCase):
         """
         Test after date for floating holiday
         """
+        
+        calendar = FinancialDate.Calendar()
+
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(2002, 3, 29), 0), datetime.datetime(2002, 4, 1))    
+        self.assertEqual(calendar.FirstTradingDayAfter(datetime.datetime(2002, 3, 29)), datetime.datetime(2002, 4, 1))
 
         self.assertEqual(FinancialDate.NthTradingDayAfter(datetime.datetime(2002, 3, 29), 0), datetime.datetime(2002, 4, 1))
         self.assertEqual(FinancialDate.FirstTradingDayAfter(datetime.datetime(2002, 3, 29)), datetime.datetime(2002, 4, 1))
@@ -73,12 +108,20 @@ class FinancialDateUnitTest(unittest.TestCase):
         Test combination of non-trivial N and static holiday in non-recent history for before dates
         """
         
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.NthTradingDayBefore(datetime.datetime(1978, 12, 25), 7), datetime.datetime(1978, 12, 13))
+        
         self.assertEqual(FinancialDate.NthTradingDayBefore(datetime.datetime(1978, 12, 25), 7), datetime.datetime(1978, 12, 13))
             
     def test_7_after_1978_christmas(self):
         """
         Test combination of non-trivial N and static holiday in non-recent history for after dates
         """
+        
+        calendar = FinancialDate.Calendar()
+        
+        self.assertEqual(calendar.NthTradingDayAfter(datetime.datetime(1978, 12, 25), 7), datetime.datetime(1979, 1,  5))
         
         self.assertEqual(FinancialDate.NthTradingDayAfter(datetime.datetime(1978, 12, 25), 7), datetime.datetime(1979, 1,  5))
         
@@ -87,8 +130,16 @@ class FinancialDateUnitTest(unittest.TestCase):
         Test inclusion of specific trading days
         """
         
-        self.assertTrue(FinancialDate.IsTradingDay(datetime.datetime(1965, 8, 17)))
+        calendar = FinancialDate.Calendar()
         
+        self.assertTrue(calendar.IsTradingDay(datetime.datetime(1965, 8, 17)))
+        self.assertFalse(calendar.IsTradingDay(datetime.datetime(2011, 5, 22)))
+        
+#        self.assertTrue(calendar.IsTradingDay(datetime.date(1965, 8, 17)))
+#        self.assertFalse(calendar.IsTradingDay(datetime.date(2011, 5, 22)))
+        
+        self.assertTrue(FinancialDate.IsTradingDay(datetime.datetime(1965, 8, 17)))
+        self.assertFalse(FinancialDate.IsTradingDay(datetime.datetime(2011, 5, 22)))
         
 if __name__ == "__main__": #for coverage tests
 	unittest.main()
